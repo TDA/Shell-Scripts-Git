773136046db573a81816bc63936f978442d5c848
Sai Pc <schand31@asu.edu>
2015-12-21 20:14:04 +0530

    autopusher complete, i think

--- AutoPusher.py
+++ AutoPusher.py
@@ -6,6 +6,9 @@ import subprocess
 def checkFilesForPush(dirName, fileNamePattern):
     os.chdir(dirName)
     folderList=os.listdir(r"./")
+    count = 0
+    reposCount = 0
+    reposToPush = []
 
     # iterate thru each folder and check for the repos that we need to push
     for folderName in folderList:
@@ -14,14 +17,34 @@ def checkFilesForPush(dirName, fileNamePattern):
             print folderName
             os.chdir(folderName)
             # cycle through and find the repos to be pushed
-            status = subprocess.call('git s', shell=True)
-            # print status
-            # not a git repo
-            if status != 128:
-                # need to check for add/track/push
-                print "Git repo"
+            repos = os.listdir(r".")
+            for repo in repos:
+                if os.path.isdir(repo) and not repo.startswith('.'):
+                    os.chdir(repo)
+                    status = subprocess.call('git s | clear', shell=True)
+                    # print status
+                    print repo
+                    # git repo
+                    if status != 128:
+                        # need to check for add/track/push
+                        print "Git repo"
+                        pushStatus = subprocess.call('git s | egrep --color=auto \'(Changes to be committed)|(untrack)|(use "git push" to publish your local commits)\'', shell=True)
+                        print pushStatus
+                        if pushStatus == 0:
+                            print "Found Repo to add/track/push"
+                            reposToPush.append(repo)
+                            reposCount += 1
+                    else:
+                        # not a git repo
+                        print "Not a Git repo"
+                    # go back to parent and continue
+                    os.chdir(r"../")
+                    count += 1
             # go back to parent and continue
             os.chdir(r"../")
+    print count, "folders checked"
+    print reposCount, "repos found"
+    print reposToPush
     #
     #     # theoretically this should work, it worked for the file i compiled myself to a.out
     #     # now if we can have all the .out files in the directories, we can simply check for all mem leaks :D
@@ -32,4 +55,4 @@ def checkFilesForPush(dirName, fileNamePattern):
 
 
 
-checkFilesForPush(r"../", "Projects")
+checkFilesForPush(r"../", "RubymineProjects")
