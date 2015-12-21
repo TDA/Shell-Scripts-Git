d642146d418d222c12de99ba7510531ea05f589c
Sai Pc <schand31@asu.edu>
2015-12-21 19:49:53 +0530

    auto push hook for git- in progress

--- AutoPusher.py
+++ AutoPusher.py
@@ -3,19 +3,24 @@ __author__ = 'saipc'
 import os
 import subprocess
 
-def checkFilesForPush(dirName):
+def checkFilesForPush(dirName, fileNamePattern):
     os.chdir(dirName)
     folderList=os.listdir(r"./")
 
     # iterate thru each folder and check for the repos that we need to push
     for folderName in folderList:
         # cycle through the project folders
-        if os.path.isdir(folderName) and folderName.endswith("Projects"):
+        if os.path.isdir(folderName) and folderName.endswith(fileNamePattern):
             print folderName
             os.chdir(folderName)
             # cycle through and find the repos to be pushed
             status = subprocess.call('git s', shell=True)
-            print status
+            # print status
+            # not a git repo
+            if status != 128:
+                # need to check for add/track/push
+                print "Git repo"
+            # go back to parent and continue
             os.chdir(r"../")
     #
     #     # theoretically this should work, it worked for the file i compiled myself to a.out
@@ -27,4 +32,4 @@ def checkFilesForPush(dirName):
 
 
 
-checkFilesForPush(r"../")
+checkFilesForPush(r"../", "Projects")
