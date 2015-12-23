884eadcd037839ca05e0ac0ddb7799700afe6b38
Sai Pc <schand31@asu.edu>
2015-12-23 22:23:22 +0530

    More enhancements for finding and pushing by itself

--- AutoPusher.py
+++ AutoPusher.py
@@ -2,57 +2,61 @@ __author__ = 'saipc'
 
 import os
 import subprocess
+import re
+
+
+reposCount = 0
+reposToPush = []
+count = 0
 
 def checkFilesForPush(dirName, fileNamePattern):
+    print "Searching from ", dirName
     os.chdir(dirName)
     folderList=os.listdir(r"./")
-    count = 0
-    reposCount = 0
-    reposToPush = []
+    fileNamePattern = re.compile(fileNamePattern)
 
     # iterate thru each folder and check for the repos that we need to push
     for folderName in folderList:
         # cycle through the project folders
-        if os.path.isdir(folderName) and folderName.endswith(fileNamePattern):
-            print folderName
+        if os.path.isdir(folderName) and re.search(fileNamePattern, folderName):
+            print "Searching ", folderName
             os.chdir(folderName)
-            # cycle through and find the repos to be pushed
+            # if this itself is a git repo, push
+            status = subprocess.call('git s', shell=True)
+            if status != 128:
+                push(folderName)
+            # else cycle through and find the repos to be pushed
             repos = os.listdir(r".")
             for repo in repos:
                 if os.path.isdir(repo) and not repo.startswith('.'):
                     os.chdir(repo)
-                    status = subprocess.call('git s | clear', shell=True)
+                    status = subprocess.call('git s', shell=True)
                     # print status
-                    print repo
-                    # git repo
                     if status != 128:
-                        # need to check for add/track/push
-                        print "Git repo"
-                        pushStatus = subprocess.call('git s | egrep --color=auto \'(Changes to be committed)|(untrack)|(use "git push" to publish your local commits)\'', shell=True)
-                        print pushStatus
-                        if pushStatus == 0:
-                            print "Found Repo to add/track/push"
-                            reposToPush.append(repo)
-                            reposCount += 1
+                        # git repo
+                        # need to push
+                        push(repo)
                     else:
                         # not a git repo
                         print "Not a Git repo"
                     # go back to parent and continue
                     os.chdir(r"../")
-                    count += 1
+                    # count += 1
             # go back to parent and continue
             os.chdir(r"../")
     print count, "folders checked"
     print reposCount, "repos found"
     print reposToPush
     #
-    #     # theoretically this should work, it worked for the file i compiled myself to a.out
-    #     # now if we can have all the .out files in the directories, we can simply check for all mem leaks :D
-    #     filePath = os.path.join(dirName, folderName, "a.out")
-    #     fullFilePathCommand= r"/Users/schandramouli/PycharmProjects/Python_AutomatedCorrectors/script.sh " + filePath
-    #     retVal = subprocess.call('bash ' + fullFilePathCommand, shell=True)
-    #     print retVal
 
+def push(repo):
+    print "Git repo"
+    pushStatus = subprocess.call('git s | egrep --color=auto \'Your branch is up-to-date\'', shell=True)
+    # print pushStatus
+    if pushStatus == 1:
+        # means not up to date
+        print "This repo is not up to date", repo
+        reposToPush.append(repo)
+        #reposCount += 1
 
-
-checkFilesForPush(r"../", "RubymineProjects")
+checkFilesForPush(r"../", r"(Projects)|(Scripts)$")
